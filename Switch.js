9f2f927b87d7e3cff4c7ff3c02839cb04ff3c342
SamnBierle <sam.nbierle@gmail.com>
2023-02-06 23:31:24 -0600

    Working Style Switching

---
+++ Switch.js
@@ -0,0 +1,33 @@
+var num = 0; 
+
+function button1() {
+    num++;
+    document.getElementById("output").innerHTML = "Press x " + num;
+}
+
+function toggleStyleSheet(){
+
+    const styleElement = document.getElementById("mainStyleSheet");
+    var filename = styleElement.getAttribute("href");
+
+    var newFilename = "style2.css";
+    if (filename == "style.css") {
+        newFilename = "style2.css";
+    } else {
+        newFilename = "style.css";
+    }
+
+    styleElement.setAttribute("href", newFilename);
+    localStorage.setItem("stylesheet", newFilename);
+}
+
+
+window.onload = function(){
+
+    var filename = localStorage.getItem("stylesheet");
+    var styleElement = document.getElementById("mainStyleSheet");
+    styleElement.setAttribute("href", filename);
+    
+}
+
+
